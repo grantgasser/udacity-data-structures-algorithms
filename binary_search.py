6bb797f8c1f762918f477f8ca385d702701bd14a
Grant Gasser <glgasser@gmail.com>
2019-07-29 11:38:01 -0700

    Implement binary search.

--- binary_search.py
+++ binary_search.py
@@ -14,7 +14,19 @@ Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
 def binary_search(input_array, value):
-    """Your code goes here."""
+    low = 0
+    high = len(input_array) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if value == input_array[mid]:
+            return mid
+        elif value > input_array[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
     return -1
 
 test_list = [1,3,9,11,15,19,29]
